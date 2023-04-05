cc3f75b50efdb224d652c8193e2f775508727b0f
zthomps99 <zthomps@iu.edu>
2023-04-05 18:27:32 -0400

    Game details

--- view/game/detail/game_detail.class.php
+++ view/game/detail/game_detail.class.php
@@ -1,7 +1,30 @@
 <?php
 /**
- * Author: Val Cinbat
+ * Author: Zach Thompson
  * Date: 4/4/2023
  * File: game_detail.class.php
- * Description:
+ * Description: display game details
  */
+class GameDetails extends GameIndexView {
+
+    public function display($game, $confirm = "") {
+        //display page header
+        parent::displayHeader("Game Details");
+
+        //retrieve game details by getting get methods
+        $id = $game->getId();
+        $title = $game->getTitle();
+        $genre = $game->getGenre();
+        $platform = $game->getPlatform();
+        $release_date =  new \DateTime($game->getRelease_date());
+        $description = $game->getDescription();
+        $image = $game->getImage();
+        if (strpos ($image, "http://") === false AND strpos($image, "https://") === false) {
+            $image = BASE_URL . "/" . GAME_IMG . $image;
+        }
+        ?>
+
+        <div id="main-header">Game Details</div>
+        <hr>
+    }
+}
--- view/game/index/game_index.class.php
+++ view/game/index/game_index.class.php
@@ -3,5 +3,43 @@
  * Author: Zach Thompson
  * Date: 4/4/2023
  * File: game_index.class.php
- * Description:
+ * Description: defines the display method, which displays games in the designated format (6 games in a row)
  */
+
+//displays an array of games in a grid format
+class GameIndex extends GameIndexView {
+    public function display($games){
+    //display page header
+    parent::displayHeader("List All Games");
+    ?>
+    <div id="main-header"> Game Library</div>
+
+    <div class="grid-container">
+        <?php
+        if ($games == 0) {
+            echo "No game was found. <br><br><br><br>">
+        } else {
+        //displays games in a grid; six games per row
+        foreach ($games as $game) {
+            $id = $game->getId();
+            $title = $game->getTitle();
+            $genre = $game->getGenre();
+            $platform = $game->getPlatform();
+            $release_date =  new \DateTime($game->getRelease_date());
+            $description = $game->getDescription();
+            $image = $game->getImage();
+            if (strpos ($image, "http://") === false AND strpos($image, "https://") === false) {
+                $image = BASE_URL . "/" . GAME_IMG . $image;
+            }
+
+            echo "<div class='item'><p><a href='", BASE_URL, "/game/detail/$id'><img src='" . $image . "'></a>
+            <span>$title<br>Genre $genre<br>Platform $platform<br>" . $release_date->format('m-d-y') . "</span></p></div>";
+            }
+        }
+        ?>
+    </div>
+    <?php
+    //display page footer
+    parent::displayFooter();
+    }
+}
