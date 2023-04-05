b05d0f6724dad42dcf604922713afe7f6880cbcd
vcinbat <122567462+vcinbat@users.noreply.github.com>
2023-04-05 12:11:46 -0400

    Added config.php file and created the database class and dispatcher class.

---
+++ application/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Author: Val Cinbat
+ * Date: 4/5/2023
+ * File: config.php
+ * Description: Sets the application settings.
+ */
+
+//error reporting level: 0 to turn off all error reporting; E_ALL to report all
+error_reporting(E_ALL);
+
+//local time zone
+date_default_timezone_set('America/New_York');
+
+//base url of the application
+define("BASE_URL", "https://localhost/FinalProject");
+
+
+/*************************************************************************************
+ *                       settings for games                                        *
+ ************************************************************************************/
+
+//define default path for media images
+define("GAME_IMG", "www/img/games");
--- application/database.class.php
+++ application/database.class.php
@@ -3,5 +3,72 @@
  * Author: Val Cinbat
  * Date: 4/4/2023
  * File: database.class.php
- * Description:
+ * Description: The database class sets the database details
  */
+
+class Database
+{
+
+    //define database parameters
+    private $param = array(
+        'host' => 'localhost',
+        'login' => 'phpuser',
+        'password' => 'phpuser',
+        'database' => 'game_inventory',
+        'tblGames' => 'games',
+        'tblFavorites' => 'favorites',
+        'tblUsers' => 'users',
+        'tblRoles' => 'roles'
+    );
+
+    //define the database connection object
+    private $objDBConnection = NULL;
+    static private $_instance = NULL;
+
+    //constructor
+    public function __construct() {
+
+        $this->objDBConnection = @new mysqli(
+            $this->param['host'],
+            $this->param['login'],
+            $this->param['password'],
+            $this->param['database']
+        );
+        if (mysqli_connect_errno() != 0) {
+            exit("Connecting to database failed: " . mysqli_connect_error());
+        }
+    }
+
+    //static method to ensure there is just one Database instance
+    static public function getInstance() {
+        if (self::$_instance == NULL) {
+            self::$_instance = new Database();
+        }
+        return self::$_instance;
+    }
+
+    //this function returns the database connection object
+    public function getConnection() {
+        return $this->objDBConnection;
+    }
+
+    //returns the name of the table storing books
+    public function getGamesTable() {
+        return $this->param['tblGames'];
+    }
+
+    //returns the name of the table storing books
+    public function getFavoritesTable() {
+        return $this->param['tblFavorites'];
+    }
+
+    //returns the name of the table storing books
+    public function getUsersTable() {
+        return $this->param['tblUsers'];
+    }
+
+    //returns the name of the table storing books
+    public function getRoles() {
+        return $this->param['tblRoles'];
+    }
+}
